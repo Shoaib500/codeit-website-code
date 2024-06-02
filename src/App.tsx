a799ce89d269ed5c96aedc6e580fb85e63fd4039
aman123424 <ch20b010@smail.iitm.ac.in>
2024-06-02 22:25:20 +0530

    logo changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./screens/home/home";
 import Navbar from "./components/navbar/navbar";
 import Footer from "./components/footer/footer";
+import About from "./screens/about/about";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about-us" element={<About/>}/>
         </Routes>
       </HashRouter>
     </div>
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,33 +1,103 @@
-import React, { useState } from 'react';
-import './navbar.scss'
-import logo from '../../assets/logos/logo.png'
+import React, { useState } from "react";
+import "./navbar.scss";
+import logo from "../../assets/logos/fullLogo.jpg";
+import downArrow from "../../assets/icons/downArrow.svg";
+import { useNavigate } from "react-router-dom";
 
-type NavbarProps = {
-    
-};
+type NavbarProps = {};
 
-const Navbar:React.FC<NavbarProps> = () => {
+const Navbar: React.FC<NavbarProps> = () => {
+  const [activeTab, setActiveTab] = useState<string>("home");
+  const [showMenu, setShowMenu] = useState<boolean>(false)
+  const navigate = useNavigate();
 
-    const [activeTab, setActiveTab] = useState<string>("home");
-    
-    return(
-        <div className='navbar'>
-            <div className='left'>
-                <img src={logo} alt="" className="logo" onClick={()=>{setActiveTab("home")}}/>
+
+
+  return (
+    <div className="navbar">
+      <div className="left">
+        <img
+          src={logo}
+          alt=""
+          className="logo"
+          onClick={() => {
+            setActiveTab("home");
+            navigate("/");
+          }}
+        />
+      </div>
+      <div className="mid">
+        <div className="nav-elements">
+          <div
+            className={`nav-element ${
+              activeTab === "about-us" ? "active" : ""
+            }`}
+            onClick={() => {
+              setActiveTab("about-us");
+              navigate("/about-us");
+            }}
+          >
+            About Us
+          </div>
+
+          <div className="dropdown-wrapper" onMouseEnter={()=>{setShowMenu(true)}} onMouseLeave={()=>{setShowMenu(false)}}>
+            <div className={`nav-elem-wrapper`}>
+              <div
+                className={`nav-element ${
+                  activeTab === "services" ? "active" : ""
+                }`}
+                onClick={() => {
+                  setActiveTab("services");
+                }}
+              >
+                Services
+              </div>
+              <img className="arrow" src={downArrow} alt="" />
             </div>
-            <div className="mid">
-                <div className="nav-elements">
-                    <div className={`nav-element ${activeTab === "about-us" ? "active" : ""}`} onClick={()=>{setActiveTab("about-us")}}>About Us</div>
-                    <div className={`nav-element ${activeTab === "services" ? "active" : ""}`} onClick={()=>{setActiveTab("services")}}>Services</div>
-                    <div className={`nav-element ${activeTab === "contact-us" ? "active" : ""}`} onClick={()=>{setActiveTab("contact-us")}}>Contact Us</div>
-                    <div className={`nav-element ${activeTab === "portfolio" ? "active" : ""}`} onClick={()=>{setActiveTab("portfolio")}}>Portfolio</div>
-                    <div className={`nav-element ${activeTab === "blog" ? "active" : ""}`} onClick={()=>{setActiveTab("blog")}}>Blog</div>
+{/* 
+            <div className={`dropdown-menu ${showMenu ? "show" : ""}`}>
+                <div className="header">
+                    <div className="title">Design & Development</div>
+                    <div className="title">Digital Marketing</div>
+                    <div className="title">Content Writing</div>
                 </div>
-            </div>
-            <div className="right">
-                <div className="speak-to-expert-btn">Speak to an Expert</div>
-            </div>
+            </div> */}
+          </div>
+
+          <div
+            className={`nav-element ${
+              activeTab === "contact-us" ? "active" : ""
+            }`}
+            onClick={() => {
+              setActiveTab("contact-us");
+            }}
+          >
+            Contact Us
+          </div>
+          <div
+            className={`nav-element ${
+              activeTab === "portfolio" ? "active" : ""
+            }`}
+            onClick={() => {
+              setActiveTab("portfolio");
+            }}
+          >
+            Portfolio
+          </div>
+          <div
+            className={`nav-element ${activeTab === "blog" ? "active" : ""}`}
+            onClick={() => {
+              setActiveTab("blog");
+            }}
+          >
+            Blog
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="right">
+        <div className="speak-to-expert-btn">Speak to an Expert</div>
+      </div>
+    </div>
+  );
+};
 export default Navbar;
